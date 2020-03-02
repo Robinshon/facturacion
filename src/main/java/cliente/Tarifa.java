88d20be15d0fe3108d033d24f88b299d8ac30df2
Robinshon <robertneagu58@gmail.com>
2020-03-02 16:00:05 +0100

    Proyecto base y test modificacion

---
+++ src/main/java/cliente/Tarifa.java
@@ -0,0 +1,11 @@
+package cliente;
+
+public class Tarifa {
+    private double precioPorSegundo;
+    public Tarifa(double precioPorSegundo){
+        this.precioPorSegundo = precioPorSegundo;
+    }
+    public double getPrecioPorSegundo() {
+        return  precioPorSegundo;
+    }
+}
