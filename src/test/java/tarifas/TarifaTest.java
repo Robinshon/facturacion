a3b6b5de3549d8179dc8826e85aa7b98fb2ef889
Robinshon <robertneagu58@gmail.com>
2020-04-18 12:42:45 +0200

    sesion 1,2,3 y test funcionando

--- src/test/java/tarifas/TarifaTest.java
+++ src/test/java/tarifas/TarifaTest.java
@@ -15,18 +15,12 @@ class TarifaTest {
     @BeforeEach
     public void init() {
         Calendar fecha = Calendar.getInstance();
-        fecha.set(Calendar.DAY_OF_WEEK,1);
+        fecha.set(Calendar.DAY_OF_WEEK,7);
         llamada = new Llamada("655847852",fecha,500);
         Calendar fecha2 = Calendar.getInstance();
+        fecha2.set(Calendar.DAY_OF_WEEK,1);
         fecha2.set(Calendar.HOUR_OF_DAY,17);
         llamada2 = new Llamada("666445874",fecha2,600);
-        Calendar fecha3 = Calendar.getInstance();
-        fecha3.set(Calendar.DAY_OF_WEEK,1);
-        fecha3.set(Calendar.HOUR_OF_DAY, 18);
-
-        Calendar fecha4 = Calendar.getInstance();
-        fecha4.set(Calendar.DAY_OF_WEEK,6);
-
         tarifa = new TarifaBasica(10);
     }
 
@@ -41,7 +35,7 @@ class TarifaTest {
     public void testCalcularImporte() {
         tarifa = new TarifaDomingo(tarifa, 0);
         assertEquals(0,tarifa.calcularImporte(llamada));
-        tarifa = new TarifaTarde(tarifa,5);
-        assertEquals(3000,tarifa.calcularImporte(llamada2));
+        tarifa = new TarifaTarde(tarifa,3);
+        assertEquals(30,tarifa.calcularImporte(llamada2));
     }
 }
